70a44ec72d016fd08e52bb3f40495b4fe1baa1cf
Priyash Patil <38959321+priyashpatil@users.noreply.github.com>
2024-10-17 16:02:33 +0530

    fix: mac install script

--- install/mac.sh
+++ install/mac.sh
@@ -22,12 +22,12 @@ fi
 RELEASE_TAG=$(curl -s "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases/latest" |
     grep -o '"tag_name": "[^"]*' | sed 's/"tag_name": "//')
 
+# Echo which version is being downloaded
+echo -e "Downloading phpup version $RELEASE_TAG \n"
+
 # Get the download URL for the asset
 DOWNLOAD_URL=$(curl -s "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases/tags/$RELEASE_TAG" |
     grep -o "https://github.com/$REPO_OWNER/$REPO_NAME/releases/download/$RELEASE_TAG/$ASSET_NAME")
-
-# Echo which version is being downloaded
-echo "Downloading phpup version $RELEASE_TAG"
 
 # Download the asset
 curl -L -o "$ASSET_NAME" "$DOWNLOAD_URL"
@@ -36,6 +36,7 @@ curl -L -o "$ASSET_NAME" "$DOWNLOAD_URL"
 chmod +x "$ASSET_NAME"
 
 # Move the binary to a directory in PATH
-# sudo mv "$ASSET_NAME" /usr/local/bin/phpup
+echo -e "\nInstalling phpup to /usr/local/bin/phpup"
+sudo mv "$ASSET_NAME" /usr/local/bin/phpup
 
-echo "phpup has been installed successfully!"
+echo -e "\nphpup has been installed successfully! 🎉"
